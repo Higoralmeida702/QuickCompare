8b38bc1976afd20c1188266b47e4885d82f7f724
Higoralmeida702 <higoralmeida7022@gmail.com>
2025-05-03 00:22:21 -0300

    feat: criado ApplicationDbContext
    -Mapeiado a propriedade de valor 'Tela' como um objeto embutido (OwnsOne).

---
+++ Backend/QuickCompare.Infra.Data/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using QuickCompare.Domain.Entity;
+
+namespace QuickCompare.Infra.Data.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<CelularEntity> Celulares { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<CelularEntity>()
+                .OwnsOne(x => x.Tela);
+        }
+    }
+}
