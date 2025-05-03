3a55178bd2fae3d9b88c23141c44d11f1fab4dfa
Higoralmeida702 <higoralmeida7022@gmail.com>
2025-05-03 01:05:14 -0300

    chore: configura conexão com MySQL e adiciona Swagger
    -Configurado a conexão com MySQL no ApplicationDbContext usando o connection string do appsettings.
    -Registra serviços de Celular (Service e Repository) na DI.
    -Adicionado configuração do Swagger para gerar a documentação da API.

--- Backend/QuickCompare.Api/Program.cs
+++ Backend/QuickCompare.Api/Program.cs
@@ -1,16 +1,46 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using QuickCompare.Application.Interfaces;
+using QuickCompare.Application.Services;
+using QuickCompare.Domain.Interfaces;
+using QuickCompare.Infra.Data.Data;
+using QuickCompare.Infra.Data.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+
+var serverVersion = new MySqlServerVersion(new Version(8, 0, 41));
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql(builder.Configuration.GetConnectionString("defaultConnection"), serverVersion));
+
+builder.Services.AddScoped<ICelularService, CelularService>();
+builder.Services.AddScoped<ICelularRepository, CelularRepository>();
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "QuickCompare API",
+        Version = "v1"
+    });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
 app.Run();
