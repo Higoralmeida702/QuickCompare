1531b0e90c5b54822facbb1d972d523d8a75f660
Higoralmeida702 <higoralmeida7022@gmail.com>
2025-05-08 15:46:29 -0300

    feat: adicionado NotebookDto
    -Criado a classe NotebookDto na camada Application para representar os dados de entrada do notebooks.

---
+++ Backend/QuickCompare.Application/Dto/NotebookDto.cs
@@ -0,0 +1,31 @@
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+using QuickCompare.Domain.Entity;
+using QuickCompare.Domain.Enum;
+
+namespace QuickCompare.Application.Dto
+{
+    public class NotebookDto
+    {
+        public string Processador { get; set; }
+        public string PlacaDeVideo { get; set; }
+        public int QuantidadePortasUsb20 { get; set; }
+        public int QuantidadePortasUsb32 { get; set; }
+        public bool EntradaC { get; set; }
+        public bool MemoriaRamExpansivel { get; set; }
+        public bool CapacidadeArmazenamentoExpansivel { get; set; }
+        public bool WebCam { get; set; }
+        public GeracaoMemoriaRam GeracaoMemoriaRam { get; set; }
+        public FrequenciaMemoriaRam FrequenciaMemoriaRam { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int? Espessura { get; set; }
+        public int? Peso { get; set; }
+        public int? Mah { get; set; }
+        public int? DuracaoBateriaAproximada { get; set; }
+        public CapacidadeArmazenamento CapacidadeArmazenamento { get; set; }
+        public MemoriaRam MemoriaRam { get; set; }
+        public TipoAparelho TipoAparelho { get; set; }
+        public TelaEntity Tela { get; set; }
+    }
+}
